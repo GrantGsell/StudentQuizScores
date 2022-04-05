03649a0887b55ea25625d98c964d588f881307e3
GrantGsell <47372698+GrantGsell@users.noreply.github.com>
2022-04-05 15:31:44 -0400

    Initial project creation

---
+++ StudentQuizScores/src/main/java/com/mycompany/studentquizscores/StudentQuizScores.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.studentquizscores;
+
+/**
+ *
+ * @author Grant
+ */
+public class StudentQuizScores {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
